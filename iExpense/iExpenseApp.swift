bc513bf4c6819e85b324a24270242c7905d9b5a2
Saverio Negro <saverionegro@gmail.com>
2024-11-26 12:46:10 +0100

    Initial commit.

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Saverio Negro on 26/11/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
