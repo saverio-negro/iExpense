b10d9c3df1d7ca3fb5bf352241af4cbb223f6fb6
Saverio Negro <saverionegro@gmail.com>
2024-11-30 10:58:59 +0100

    Create  struct to represent the expense item from the user.

---
+++ iExpense/Models/ExpenseItem.swift
@@ -0,0 +1,13 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Saverio Negro on 30/11/24.
+//
+
+// Struct representing the single expense item
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: String
+}
